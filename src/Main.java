aa42b82f8ff0dd1056796533c63199f594bb45c3
sasa1978 <Maximus.78@mail.ru>
2022-02-21 19:21:37 +0300

    Edit dot's

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
         for (int i = 0; i < 10_000_000; i++) {
-        //for (int i = 0; i < 10; i++) {
+            //for (int i = 0; i < 10; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -18,23 +18,23 @@ public class Main {
         }
         //persons.stream().forEach(System.out::println);
 
-        long underageCount = persons.stream().
-                filter(person -> person.getAge() < 18).
-                count();
+        long underageCount = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
         System.out.println("Людей младше 18 лет: " + underageCount);
 
-        List<String> recruit = persons.stream().
-                filter(person -> person.getAge() >= 18 && person.getAge() <= 27).
-                map(Person::getFamily).
-                collect(Collectors.toList());
+        List<String> recruit = persons.stream()
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
         System.out.println("Людей призывного возраста: " + recruit.size());
         //recruit.stream().forEach(System.out::println);
 
-        List<Person> higherEducation = persons.stream().
-                filter(person -> person.getEducation() == Education.HIGHER &&
-                        person.getAge() >= 18 && (person.getSex() == Sex.MAN ? person.getAge() <= 65 : person.getAge() <= 60)).
-                sorted(Comparator.comparing(Person::getFamily)).
-                collect(Collectors.toList());
+        List<Person> higherEducation = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER &&
+                        person.getAge() >= 18 && (person.getSex() == Sex.MAN ? person.getAge() <= 65 : person.getAge() <= 60))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
         System.out.println("Людей рабочего возраста с высшим образованием: " + higherEducation.size());
         //higherEducation.stream().forEach(System.out::println);
     }
